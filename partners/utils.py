75db4f3abb2cc8b86eaacd94779524274f6625ee
Edward Gomez <edward@gomez.me.uk>
2021-05-04 08:45:32 +0100

    Basic PDF functions

---
+++ partners/utils.py
@@ -0,0 +1,47 @@
+import functools
+
+from django.conf import settings
+from django.views.generic import DetailView
+
+from django_weasyprint import WeasyTemplateResponseMixin
+from django_weasyprint.views import CONTENT_TYPE_PNG, WeasyTemplateResponse
+
+from partners.models import Proposal
+
+class MyModelView(DetailView):
+    # vanilla Django DetailView
+    model = Proposal
+    template_name = 'proposal_print.html'
+
+class CustomWeasyTemplateResponse(WeasyTemplateResponse):
+    # customized response class to change the default URL fetcher
+    def get_url_fetcher(self):
+        # disable host and certificate check
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+        return functools.partial(django_url_fetcher, ssl_context=context)
+
+class MyModelPrintView(WeasyTemplateResponseMixin, MyModelView):
+    # output of MyModelView rendered as PDF with hardcoded CSS
+    pdf_stylesheets = [
+        settings.STATIC_ROOT + 'css/app.css',
+    ]
+    # show pdf in-line (default: True, show download dialog)
+    pdf_attachment = False
+    # custom response class to configure url-fetcher
+    response_class = CustomWeasyTemplateResponse
+
+class MyModelDownloadView(WeasyTemplateResponseMixin, MyModelView):
+    # suggested filename (is required for attachment/download!)
+    pdf_filename = 'foo.pdf'
+
+class MyModelImageView(WeasyTemplateResponseMixin, MyModelView):
+    # generate a PNG image instead
+    content_type = CONTENT_TYPE_PNG
+
+    # dynamically generate filename
+    def get_pdf_filename(self):
+        return 'foo-{at}.pdf'.format(
+            at=timezone.now().strftime('%Y%m%d-%H%M'),
+        )
